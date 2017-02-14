e28d8c4f026a305d3a366e6bd9b3abe2980dfb87
Matt Brophy <mbrophy@urbn.com>
2017-02-13 21:24:02 -0500

    Check git status differently

--- deploy.sh
+++ deploy.sh
@@ -38,7 +38,7 @@ git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if git diff --quiet ; then
+if [ $(git status --porcelain | wc -l) -lt 1 ]; then
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
